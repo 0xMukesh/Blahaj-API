66e9851abe3d9fc9e9b6aa00fd12502906f97a62
Kira <mukeshreddy.9.new@gmail.com>
2022-01-15 18:10:53 +0530

    fix: add all routes to index.js file

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const router = require("./routes/router.js");
+const {
+    getImages,
+    postImages
+} = require("./controllers/controllers.js")
 
 const app = express();
 
@@ -22,13 +26,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(router);
-
 const port = process.env.PORT || 80
 
 app.get('/', (req, res) => {
     res.send('🦈 BLAHAJ API!')
 })
+
+app.post('/post', postImages)
+app.get('/get', getImages)
 
 app.listen(port, async () => {
     console.log(`Server is online at ${port} port`);
